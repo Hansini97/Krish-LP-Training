ea8209943803144d36a861a495caecb9eebc0290
Hansini97 <53207797+Hansini97@users.noreply.github.com>
2021-05-24 00:44:39 +0530

    Update CommunicationManager.java

--- Singleton/CommunicationManager.java
+++ Singleton/CommunicationManager.java
@@ -1,1 +1,26 @@
+public class CommunicationManager {
+
+    private static volatile CommunicationManager comManager;
+
+    private CommunicationManager(){
+        if(comManager != null){
+            throw new RuntimeException("Please use getComManager method");
+        }
+
+    }
+
+    public static CommunicationManager getComManager(){
+
+        if(comManager == null){
+            synchronized (CommunicationManager.class){
+                if(comManager == null){
+                    comManager = new CommunicationManager();
+                }
+            }
+
+        }
+
+        return comManager;
+    }
+}
 
