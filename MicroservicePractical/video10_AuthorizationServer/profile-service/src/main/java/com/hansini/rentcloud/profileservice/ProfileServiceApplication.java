55bd7420aac92646f59f27c6579ab1794daf0b2d
Hansini97 <hansinirup@gmail.com>
2021-06-18 04:30:26 +0530

    video10 commit

---
+++ MicroservicePractical/video10_AuthorizationServer/profile-service/src/main/java/com/hansini/rentcloud/profileservice/ProfileServiceApplication.java
@@ -0,0 +1,17 @@
+package com.hansini.rentcloud.profileservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+@SpringBootApplication
+@EntityScan(basePackages = "com.hansini.rentcloud.commons.model")
+@EnableResourceServer
+public class ProfileServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfileServiceApplication.class, args);
+	}
+
+}
---
+++ MicroservicePractical/video10_AuthorizationServer/profile-service/src/main/java/com/hansini/rentcloud/profileservice/service/CustomerServiceImpl.java
@@ -0,0 +1,37 @@
+package com.hansini.rentcloud.profileservice.service;
+
+import com.hansini.rentcloud.commons.model.Customer;
+import com.hansini.rentcloud.profileservice.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @Override
+    public Customer save(Customer customer) {
+
+        return customerRepository.save(customer);
+
+    }
+    @Override
+    public Customer fetchById(int profileId) {
+        Optional<Customer> customer = customerRepository.findById(profileId);
+        if (customer.isPresent()) {
+            return customer.get();
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public List<Customer> fetchAllProfiles() {
+        return customerRepository.findAll();
+    }
+}
