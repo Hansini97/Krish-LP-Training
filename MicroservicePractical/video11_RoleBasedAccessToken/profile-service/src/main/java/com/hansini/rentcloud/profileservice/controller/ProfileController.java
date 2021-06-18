ccbc80c00b2906a6299f729c4606dc927ce3f6b5
Hansini97 <hansinirup@gmail.com>
2021-06-18 05:44:25 +0530

    video 11 commit

---
+++ MicroservicePractical/video11_RoleBasedAccessToken/profile-service/src/main/java/com/hansini/rentcloud/profileservice/controller/ProfileController.java
@@ -0,0 +1,34 @@
+package com.hansini.rentcloud.profileservice.controller;
+
+import com.hansini.rentcloud.commons.model.Customer;
+import com.hansini.rentcloud.profileservice.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping (value = "/services")
+public class ProfileController {
+
+    @Autowired
+    CustomerService customerService;
+
+    @RequestMapping (value = "/profile", method = RequestMethod.POST)
+    @PreAuthorize("hasAuthority('create_profile')")
+    public Customer save(@RequestBody Customer customer){
+        return customerService.save(customer);
+
+    }
+    @RequestMapping(value = "/profile", method = RequestMethod.GET)
+    public Customer fetch(@RequestParam int profileId) {
+        return customerService.fetchById(profileId);
+    }
+
+    @RequestMapping(value = "/profiles", method = RequestMethod.GET)
+    @PreAuthorize("hasRole('ROLE_operator')")
+    public List<Customer> fetch() {
+        return customerService.fetchAllProfiles();
+    }
+}
