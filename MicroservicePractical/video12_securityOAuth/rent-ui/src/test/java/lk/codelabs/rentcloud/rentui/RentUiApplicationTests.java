6e7e61e7a53f580bc4890b01bc3494f217a3fa2c
Hansini97 <hansinirup@gmail.com>
2021-06-18 13:57:51 +0530

    video12 commit

---
+++ MicroservicePractical/video12_securityOAuth/rent-ui/src/test/java/lk/codelabs/rentcloud/rentui/RentUiApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.codelabs.rentcloud.rentui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentUiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
